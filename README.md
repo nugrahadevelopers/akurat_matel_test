8a72a0ee6317b9c6ffcd4c0d27b71bf1f2b58dbc
nugrahadevelopers <nug.dev.work@gmail.com>
2021-05-10 14:44:51 +0700

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# akurat_matel
+
+Aplikasi DC Android dengan flutter
+
+## Getting Started
+
+-- Comming Soon
---
+++ android/app/src/main/kotlin/com/example/akurat_matel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.akurat_matel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
