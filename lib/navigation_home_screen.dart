7cfe350613082843e8f10aba84f3779b298e28c1
nugrahadevelopers <nug.dev.work@gmail.com>
2021-05-15 08:20:03 +0700

    Commit #2
    Action :
    -- Add navigation home screen ( on progress )
    
    Problem :
    -- Need More RAM instaled :(

---
+++ lib/navigation_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Home Screen'),
+    );
+  }
+}
