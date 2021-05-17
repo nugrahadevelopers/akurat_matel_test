5c0276f4a9ebf83c68f8c445196592c8ef2aba3c
nugrahadevelopers <nug.dev.work@gmail.com>
2021-05-17 13:38:55 +0700

    Commit #4
    Action :
    -- Remove animation
    
    To Do :
    -- Fixing detail content
    -- Add Splash Screen and Login Page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:akurat_matel/model/homelist.dart';
 import 'package:flutter/material.dart';
 import 'package:akurat_matel/app_theme.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,29 +12,36 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   List<HomeList> homeList = HomeList.homeList;
-  AnimationController animationController;
+  // AnimationController animationController;
   bool multiple = true;
 
-  @override
-  void initState() {
-    animationController = AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   animationController = AnimationController(
+  //       duration: const Duration(milliseconds: 2000), vsync: this);
+  //   super.initState();
+  // }
 
   Future<bool> getData() async {
     await Future<dynamic>.delayed(const Duration(milliseconds: 0));
     return true;
   }
 
-  @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   animationController.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
+    var maxWidth = 112.0;
+    var width = MediaQuery.of(context).size.width;
+    var columns = (width ~/ maxWidth) + 1;
+    var columnWidth = width / columns;
+//136 is the height of one grid item
+    var aspectRatio = columnWidth / 136;
+
     return Scaffold(
       backgroundColor: AppTheme.white,
       body: FutureBuilder<bool>(
@@ -65,18 +73,18 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                             children: List<Widget>.generate(
                               homeList.length,
                               (int index) {
-                                final int count = homeList.length;
-                                final Animation<double> animation =
-                                    Tween<double>(begin: 0.0, end: 1.0)
-                                        .animate(CurvedAnimation(
-                                  parent: animationController,
-                                  curve: Interval((1 / count) * index, 1.0,
-                                      curve: Curves.fastOutSlowIn),
-                                ));
-                                animationController.forward();
+                                // final int count = homeList.length;
+                                // final Animation<double> animation =
+                                //     Tween<double>(begin: 0.0, end: 1.0)
+                                //         .animate(CurvedAnimation(
+                                //   parent: animationController,
+                                //   curve: Interval((1 / count) * index, 1.0,
+                                //       curve: Curves.fastOutSlowIn),
+                                // ));
+                                // animationController.forward();
                                 return HomeListView(
-                                  animation: animation,
-                                  animationController: animationController,
+                                  // animation: animation,
+                                  // animationController: animationController,
                                   listData: homeList[index],
                                   callBack: () {
                                     Navigator.push<dynamic>(
@@ -93,9 +101,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                             gridDelegate:
                                 SliverGridDelegateWithFixedCrossAxisCount(
                               crossAxisCount: multiple ? 2 : 1,
-                              mainAxisSpacing: 12.0,
-                              crossAxisSpacing: 12.0,
-                              childAspectRatio: 1.5,
+                              mainAxisExtent: maxWidth,
+                              childAspectRatio: aspectRatio,
                             ),
                           );
                         }
@@ -170,55 +177,73 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 }
 
 class HomeListView extends StatelessWidget {
-  const HomeListView(
-      {Key key,
-      this.listData,
-      this.callBack,
-      this.animationController,
-      this.animation})
-      : super(key: key);
+  const HomeListView({
+    Key key,
+    this.listData,
+    this.callBack,
+    // this.animationController,
+    // this.animation
+  }) : super(key: key);
 
   final HomeList listData;
   final VoidCallback callBack;
-  final AnimationController animationController;
-  final Animation<dynamic> animation;
+  // final AnimationController animationController;
+  // final Animation<dynamic> animation;
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return AnimatedBuilder(
+  //     animation: animationController,
+  //     builder: (BuildContext context, Widget child) {
+  //       return FadeTransition(
+  //         opacity: animation,
+  //         child: Transform(
+  //           transform: Matrix4.translationValues(
+  //               0.0, 50 * (1.0 - animation.value), 0.0),
+  //           child: AspectRatio(
+  //             aspectRatio: 1.5,
+  //             child: ClipRRect(
+  //               borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+  //               child: Stack(
+  //                 alignment: AlignmentDirectional.center,
+  //                 children: <Widget>[
+  //                   cardDataKendaraan(),
+  //                   Material(
+  //                     color: Colors.transparent,
+  //                     child: InkWell(
+  //                       splashColor: Colors.grey.withOpacity(0.2),
+  //                       borderRadius:
+  //                           const BorderRadius.all(Radius.circular(4.0)),
+  //                       onTap: () {
+  //                         callBack();
+  //                       },
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animationController,
-      builder: (BuildContext context, Widget child) {
-        return FadeTransition(
-          opacity: animation,
-          child: Transform(
-            transform: Matrix4.translationValues(
-                0.0, 50 * (1.0 - animation.value), 0.0),
-            child: AspectRatio(
-              aspectRatio: 1.5,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: <Widget>[
-                    cardDataKendaraan(),
-                    Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        splashColor: Colors.grey.withOpacity(0.2),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(4.0)),
-                        onTap: () {
-                          callBack();
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+    return Stack(
+      children: <Widget>[
+        cardDataKendaraan(),
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            splashColor: Colors.grey.withOpacity(0.2),
+            borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+            onTap: () {
+              callBack();
+            },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 
